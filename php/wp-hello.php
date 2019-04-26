61bacfd4e323e2e8fe95abb7a3fe9f8d7185cd3e
Abuba-369 <43129621+Abuba-369@users.noreply.github.com>
2019-04-26 18:14:14 +0530

    Create wp-hello.php

---
+++ php/wp-hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Hello World!!! Good Evening!!!!,This test for tech mahidra</h1>
+
+<?php
+echo " Welcome to ZippyOPS Consulting Services LLP...!! <br> ";
+echo " It happend just now...for demo..  " ;
+echo " You are coming from zippyops...!!! ";
+?> 
+
+</body>
+</html>
